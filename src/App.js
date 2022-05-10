f093f0bb17f5c3d8fa9d138234423d61825223d0
Jfontaine <jfontaine19@gmail.com>
2022-05-09 23:28:10 -0400

    finished main app components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import Payment from "./Payment";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51KxhaiDIKdt29jUloPu1gy3uGbh7ThFqS7OtTHYPbWdfnYvTgoZpzSBASMrHVhVyXperXdLgdhhuAWEzS9Hdz5g500edW919S1"
+);
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // the user just logged in / the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route
+            path="/payment"
+            element={[
+              <Header />,
+              <Elements stripe={promise}>
+                <Payment />
+              </Elements>,
+            ]}
+          ></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/checkout" element={[<Header />, <Checkout />]}></Route>
+          <Route path="/" element={[<Header />, <Home />]}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
